55cb171be8419d31903f88f120548481cf4d2b97
Georg Augusto Schlegel <georgaugusto@gmail.com>
2023-01-02 16:44:58 -0300

    fix: Change project name and changeset

--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -6,7 +6,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@course-ignite-ui/tokens'
 import { createStitches, defaultThemeMap } from '@stitches/react'
 
 export const {
